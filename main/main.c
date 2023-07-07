860beabd8cd12e8ad6a887d801a8fd5cdf119a7c
Thunder-M-Y <917291069@qq.com>
2023-07-07 15:53:24 +0800

    successful

--- main/main.c
+++ main/main.c
@@ -33,6 +33,49 @@
 // OLED缓冲区
 uint8_t oled_buffer[128 * 64 / 8];
 
+// 示例字体数据
+const uint8_t font[][6] = {
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, // 空字符
+    {0x00, 0x00, 0x5F, 0x00, 0x00, 0x00}, // !
+    {0x00, 0x07, 0x00, 0x07, 0x00, 0x00}, // "
+    {0x14, 0x7F, 0x14, 0x7F, 0x14, 0x00}, // #
+    {0x24, 0x2A, 0x7F, 0x2A, 0x12, 0x00}, // $
+    {0x23, 0x13, 0x08, 0x64, 0x62, 0x00}, // %
+    {0x36, 0x49, 0x55, 0x22, 0x50, 0x00}, // &
+    {0x00, 0x05, 0x03, 0x00, 0x00, 0x00}, // '
+    {0x00, 0x1C, 0x22, 0x41, 0x00, 0x00}, // (
+    {0x00, 0x41, 0x22, 0x1C, 0x00, 0x00}, // )
+    {0x14, 0x08, 0x3E, 0x08, 0x14, 0x00}, // *
+    {0x08, 0x08, 0x3E, 0x08, 0x08, 0x00}, // +
+    {0x00, 0x50, 0x30, 0x00, 0x00, 0x00}, // ,
+    {0x08, 0x08, 0x08, 0x08, 0x08, 0x00}, // -
+    {0x00, 0x60, 0x60, 0x00, 0x00, 0x00}, // .
+    {0x20, 0x10, 0x08, 0x04, 0x02, 0x00}, // /
+    {0x3E, 0x51, 0x49, 0x45, 0x3E, 0x00}, // 0
+    {0x00, 0x42, 0x7F, 0x40, 0x00, 0x00}, // 1
+    {0x42, 0x61, 0x51, 0x49, 0x46, 0x00}, // 2
+    {0x21, 0x41, 0x45, 0x4B, 0x31, 0x00}, // 3
+    {0x18, 0x14, 0x12, 0x7F, 0x10, 0x00}, // 4
+    {0x27, 0x45, 0x45, 0x45, 0x39, 0x00}, // 5
+    {0x3C, 0x4A, 0x49, 0x49, 0x30, 0x00}, // 6
+    {0x01, 0x71, 0x09, 0x05, 0x03, 0x00}, // 7
+    {0x36, 0x49, 0x49, 0x49, 0x36, 0x00}, // 8
+    {0x06, 0x49, 0x49, 0x29, 0x1E, 0x00}, // 9
+    {0x00, 0x36, 0x36, 0x00, 0x00, 0x00}, // :
+    {0x00, 0x56, 0x36, 0x00, 0x00, 0x00}, // ;
+    {0x08, 0x14, 0x22, 0x41, 0x00, 0x00}, // <
+    {0x14, 0x14, 0x14, 0x14, 0x14, 0x00}, // =
+    {0x00, 0x41, 0x22, 0x14, 0x08, 0x00}, // >
+    {0x02, 0x01, 0x51, 0x09, 0x06, 0x00}, // ?
+    {0x32, 0x49, 0x79, 0x41, 0x3E, 0x00}, // @
+    {0x7E, 0x11, 0x11, 0x11, 0x7E, 0x00}, // A
+    {0x7F, 0x49, 0x49, 0x49, 0x36, 0x00}, // B
+    {0x3E, 0x41, 0x41, 0x41, 0x22, 0x00}, // C
+    {0x7F, 0x41, 0x41, 0x22, 0x1C, 0x00}, // D
+    {0x7F, 0x49, 0x49, 0x49, 0x41, 0x00}, // E
+    {0x7F, 0x09, 0x09, 0x09, 0x01, 0x00}, // F
+};
+
 // I2C初始化
 static void i2c_master_init()
 {
@@ -115,10 +158,13 @@ static void oled_set_pixel(int x, int y, uint8_t value)
 {
     if (x >= 0 && x < OLED_WIDTH && y >= 0 && y < OLED_HEIGHT)
     {
+        int page = y / 8;              // 计算页地址
+        int column = x + (page * 128); // 计算列地址
+
         if (value)
-            oled_buffer[x + (y / 8) * OLED_WIDTH] |= (1 << (y % 8));
+            oled_buffer[column] |= (1 << (y % 8));
         else
-            oled_buffer[x + (y / 8) * OLED_WIDTH] &= ~(1 << (y % 8));
+            oled_buffer[column] &= ~(1 << (y % 8));
     }
 }
 
@@ -140,18 +186,53 @@ static void oled_draw_vline(int x, int y, int height, uint8_t value)
     }
 }
 
+// 在OLED缓冲区中显示字符
+static void oled_draw_char(int x, int y, char character)
+{
+    if (x >= 0 && x < OLED_WIDTH && y >= 0 && y < OLED_HEIGHT)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            uint8_t column_data = 0;
+            if (i == 5)
+            {
+                column_data = 0x00;
+            }
+            else
+            {
+                column_data = font[character - 32][i];
+            }
+            for (int j = 0; j < 8; j++)
+            {
+                uint8_t value = (column_data >> j) & 0x01;
+                oled_set_pixel(x + i, y + j, value);
+            }
+        }
+    }
+}
+
+// 在OLED缓冲区中显示字符串
+static void oled_draw_string(int x, int y, const char *string)
+{
+    int length = strlen(string);
+    for (int i = 0; i < length; i++)
+    {
+        oled_draw_char(x + i * 6, y, string[i]);
+    }
+}
 // 更新OLED显示
 static void oled_update_display()
 {
     for (int page = 0; page < OLED_HEIGHT / 8; page++)
     {
         oled_write_command(0xB0 + page); // 设置页地址
-        oled_write_command(0x01);        // 设置低列地址  ,0x00时屏幕右侧有一条竖线乱码
+        oled_write_command(0x02);        // 设置低列地址
         oled_write_command(0x10);        // 设置高列地址
-
-        uint8_t *page_data = &oled_buffer[page * OLED_WIDTH / 8];
-
-        oled_write_data(page_data, OLED_WIDTH);
+        for (int column = 0; column < OLED_WIDTH; column++)
+        {
+            uint8_t data = oled_buffer[page * OLED_WIDTH + column];
+            oled_write_data(&data, 1);
+        }
     }
 }
 
@@ -161,14 +242,16 @@ void app_main(void)
     oled_init();       // 初始化OLED屏幕
 
     oled_clear_buffer();                                // 清空OLED缓冲区
-    oled_draw_hline(0, 0, OLED_WIDTH, 1);               // 绘制顶部水平线
-    oled_draw_hline(0, OLED_HEIGHT - 1, OLED_WIDTH, 1); // 绘制底部水平线
+    oled_draw_hline(0, 0, 127, 1);                      // 绘制顶部水平线
+    oled_draw_hline(0, 63, OLED_WIDTH, 1);              // 绘制底部水平线
     oled_draw_vline(0, 0, OLED_HEIGHT, 1);              // 绘制左侧垂直线
     oled_draw_vline(OLED_WIDTH - 1, 0, OLED_HEIGHT, 1); // 绘制右侧垂直线
+    oled_draw_string(10, 10, "123 456!");               // 在OLED屏幕中显示字符串
     oled_update_display();                              // 更新OLED显示
 
     while (1)
     {
-        vTaskDelay(1000 / portMAX_DELAY);
+
+        vTaskDelay(pdMS_TO_TICKS(I2C_MASTER_TIMEOUT_MS));
     }
 }
